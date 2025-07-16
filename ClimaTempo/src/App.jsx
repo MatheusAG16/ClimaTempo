d8f00aa93340d60eeada348effa4ea6babebc0df
Matheus de Andrade <mateusandradegermano@gmail.com>
2025-07-16 14:17:39 -0300

    Inicio da criação da interface

--- ClimaTempo/src/App.jsx
+++ ClimaTempo/src/App.jsx
@@ -1,11 +1,14 @@
 import './App.css'
+import BarraDePesquisa from './components/BarraDePesquisa'
+import ClimaInformacoes from './components/ClimaInformacoes'
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <main>
+      <BarraDePesquisa />
+      <ClimaInformacoes />
+    </main>
   )
 }
 
