f0d1f33dd65c2e9bcc26f01f99012731d2446f32
Matheus de Andrade <mateusandradegermano@gmail.com>
2025-07-16 17:05:24 -0300

    Finalização da lógica do Fetch Api
    Atualizando a aplicação para mostrar a temperatura na tela conforme o usuário busca no input.

--- ClimaTempo/src/App.jsx
+++ ClimaTempo/src/App.jsx
@@ -1,13 +1,70 @@
 import './App.css'
+import { useEffect, useState } from 'react'
 import BarraDePesquisa from './components/BarraDePesquisa'
 import ClimaInformacoes from './components/ClimaInformacoes'
 
 function App() {
 
+  ***REMOVED***
+
+  const [cidade, setCidade] = useState('')
+  const [coords, setCoords] = useState({lat: null, lon: null})
+  const [weatherData, setWeatherData] = useState(null)
+
+  // Atualiza a cidade conforme o usuário digita no input
+  const handleChange = (e) => {
+      setCidade(e.target.value)
+  }
+
+  // Pesquisa as coordenadas conforme o nome da cidade digitado
+  const handlePesquisar = (e) => {
+    e.preventDefault();
+    
+    // Requisição a API para trazer a lat e lon da cidade
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cidade}&limit=${'1'}&appid=${ApiKey}`).then(response => {
+          if(!response.ok) {
+            throw new Error("Cidade não encontrada.");
+          }
+          return response.json();
+        })
+        .then(data => {
+          if(data.length === 0) {
+            throw new Error("Nehuma coordenada encontrada.")
+          }
+          setCoords({ lat: data[0].lat, lon: data[0].lon })
+        })
+        .catch(err => {
+          console.error(err)
+        });
+  }
+
+
+  // Mostra no console as coordenadas buscadas
+  useEffect(() => {
+    if(coords.lat && coords.lon){
+      console.log('Pesquisando clima...')
+      console.log(coords.lat, coords.lon)
+
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${ApiKey}`).then(response => {
+        if(!response.ok){
+          throw new Error("Erro")
+        }
+        return(response.json())
+      })
+      .then(data => {
+        setWeatherData(data)
+      })
+    }
+  }, [coords])
+
+  if(weatherData){
+    console.log(weatherData.main.temp - 273.15)
+  }
+
   return (
     <main>
-      <BarraDePesquisa />
-      <ClimaInformacoes />
+      <BarraDePesquisa cidade={cidade} onValueChange={handleChange} onPesquisar={handlePesquisar}/>
+      {weatherData ? <ClimaInformacoes data={weatherData} cidade={cidade} /> : <p>Busque sua cidade!</p>}
     </main>
   )
 }
--- ClimaTempo/src/components/BarraDePesquisa.jsx
+++ ClimaTempo/src/components/BarraDePesquisa.jsx
@@ -1,24 +1,21 @@
 import pesquisaStyle from './barraDePesquisa.module.css'
 
-function BarraDePesquisa() {
-
-    ***REMOVED***
-
-    const handlePesquisa = () => {
-        console.log('pesquisa');
-    }
-
-    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${'Sao Paulo'}&limit=${'1'}&appid=${ApiKey}`).then(response => {
-        response.json().then(data => {
-            console.log(data[0].lat)
-            console.log(data[0].lon)
-        })
-    })
+function BarraDePesquisa({cidade, onValueChange, onPesquisar}) {
 
     return (
-        <form className={pesquisaStyle.formPesquisa}>
-            <input className={pesquisaStyle.inputPesquisa} type="text" name="inputPesquisa" id="inputPesquisa" placeholder="Digite a cidade"/>
-            <button className={pesquisaStyle.btnPesquisa} type="submit" >B</button>
+        <form className={pesquisaStyle.formPesquisa} onSubmit={onPesquisar}>
+            <input className={pesquisaStyle.inputPesquisa} 
+                type="text" 
+                name="inputPesquisa" 
+                id="inputPesquisa" 
+                placeholder="Digite a cidade" 
+                value={cidade} 
+                onChange={onValueChange}
+            />
+
+            <button className={pesquisaStyle.btnPesquisa} type="submit">
+                B
+            </button>
         </form>
     )
 }
